ed5ecb66e7f35c6125fa325a607eccf5e45619e0
August <318647@my.puhsd.org>
2022-09-14 16:45:31 -0700

    Begin Tele Op

---
+++ FtcRobotController-master/TeamCode/src/main/java/PRTeleOp.java
@@ -0,0 +1,43 @@
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name="PRTeleOp")
+public class PRTeleOp extends LinearOpMode {
+    public DcMotor leftDrive;
+    public DcMotor rightDrive;
+    public DcMotor  leftArm;
+    public Servo leftClaw;
+    public Servo rightClaw;
+
+    @Override
+    public void runOpMode() {
+        double left;
+        double right;
+        double drive;
+        double turn;
+        double clawopen;
+        double clawclose;
+        double clawup;
+        double clawdown;
+
+        leftDrive  = hardwareMap.get(DcMotor.class, "leftDrive");
+        rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
+        leftArm    = hardwareMap.get(DcMotor.class, "left_arm");
+
+        leftClaw  = hardwareMap.get(Servo.class, "left_hand");
+        rightClaw = hardwareMap.get(Servo.class, "right_hand");
+
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        drive = gamepad1.left_stick_y;
+        turn  =  gamepad1.right_stick_x;
+
+        left  = drive + turn;
+        right = drive - turn;
+    }
+
+
+}
