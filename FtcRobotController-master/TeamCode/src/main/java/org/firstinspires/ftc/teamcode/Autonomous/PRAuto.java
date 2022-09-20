0c280bfb0ed23bc913ca004a90e2b523a58fbc61
brandon sanchez <torokiki09@gmail.com>
2022-09-20 16:41:48 -0700

    update

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/PRAuto.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/PRAuto.java
@@ -5,11 +5,12 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.Hardware.Robot;
+
 @Autonomous(name="PRAuto")
 public class PRAuto extends LinearOpMode {
 
-    private DcMotor leftDrive;
-    private DcMotor rightDrive;
+    Robot PRobot = new Robot();
 
     private ElapsedTime runtime = new ElapsedTime();
 
@@ -23,5 +24,47 @@ public class PRAuto extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
+    }
+    public void encoderDrive(double speed, double leftInches, double rightInches, double timeoutS) {
+        int newFrontLeftTarget;
+        int newBackLeftTarget;
+
+        //Ensure opmode is still active
+        if (opModeIsActive()) {
+
+            //Determine new target position/send to controller
+            newFrontLeftTarget = PRobot.fL.getCurrentPosition() + (int) (leftInches * Counts_Per_In);
+            newBackLeftTarget = PRobot.bL.getCurrentPosition() + (int)(rightInches * Counts_Per_In);
+            leftDrive.setTargetPosition(newLeftTarget);
+            rightDrive.setTargetPosition(newRightTarget);
+
+            //Turns on RUN_TO_POSITION
+            leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            //resets timeout, starts motion
+            runtime.reset();
+            FrontLeftTarget.setPower(Math.abs(speed));
+            rightDrive.setPower(Math.abs(speed));
+
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (leftDrive.isBusy() && rightDrive.isBusy())) {
+
+                telemetry.addData("Running to", "%7d :%7d",
+                        leftDrive.getCurrentPosition(), rightDrive.getCurrentPosition());
+                telemetry.update();
+            }
+
+            //stop all motion
+            leftDrive.setPower(0);
+            rightDrive.setPower(0);
+
+            //Turn off RUN_TO_POSITION
+            leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            sleep(250);
+        }
     }
 }
