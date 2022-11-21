0065d57fd6fc35307e4e39dd9af5668280098543
brandon sanchez <torokiki09@gmail.com>
2022-11-21 09:34:20 -0800

    thanks genesis for copy+paste

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CVPipelines/SleeveDetection.java
@@ -0,0 +1,97 @@
+package org.firstinspires.ftc.teamcode.CVPipelines;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class SleeveDetection extends OpenCvPipeline {
+    /*
+    Orange  = Parking Left
+    Purple    = Parking Middle
+    Green = Parking Right
+     */
+
+    public enum ParkingPosition {
+        LEFT,
+        CENTER,
+        RIGHT
+    }
+
+    // TOPLEFT anchor point for the bounding box
+    private static Point SLEEVE_TOPLEFT_ANCHOR_POINT = new Point(145, 168);
+
+    // Width and height for the bounding box
+    public static int REGION_WIDTH = 80;
+    public static int REGION_HEIGHT = -120;
+
+    // Color definitions
+    private final Scalar
+            Orange  = new Scalar(254, 143, 0),
+            Purple    = new Scalar(189, 104, 250),
+            Green = new Scalar(58, 255, 5);
+
+    // Anchor point definitions
+    Point sleeve_pointA = new Point(
+            SLEEVE_TOPLEFT_ANCHOR_POINT.x,
+            SLEEVE_TOPLEFT_ANCHOR_POINT.y);
+    Point sleeve_pointB = new Point(
+            SLEEVE_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
+            SLEEVE_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
+
+    // Running variable storing the parking position
+    private volatile ParkingPosition position = ParkingPosition.LEFT;
+
+    @Override
+    public Mat processFrame(Mat input) {
+        // Get the submat frame, and then sum all the values
+        Mat areaMat = input.submat(new Rect(sleeve_pointA, sleeve_pointB));
+        Scalar sumColors = Core.sumElems(areaMat);
+
+        // Get the minimum RGB value from every single channel
+        double minColor = Math.min(sumColors.val[0], Math.min(sumColors.val[1], sumColors.val[2]));
+
+        // Change the bounding box color based on the sleeve color
+        if (sumColors.val[0] == minColor) {
+            position = ParkingPosition.CENTER;
+            Imgproc.rectangle(
+                    input,
+                    sleeve_pointA,
+                    sleeve_pointB,
+                    Purple,
+                    2
+            );
+        } else if (sumColors.val[1] == minColor) {
+            position = ParkingPosition.RIGHT;
+            Imgproc.rectangle(
+                    input,
+                    sleeve_pointA,
+                    sleeve_pointB,
+                    Green,
+                    2
+            );
+        } else {
+            position = ParkingPosition.LEFT;
+            Imgproc.rectangle(
+                    input,
+                    sleeve_pointA,
+                    sleeve_pointB,
+                    Orange,
+                    2
+            );
+        }
+
+        // Release and return input
+        areaMat.release();
+        return input;
+    }
+
+    // Returns an enum being the current position where the robot will park
+    public ParkingPosition getPosition() {
+        return position;
+    }
+}
